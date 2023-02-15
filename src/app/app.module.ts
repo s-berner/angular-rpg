c0715f215a83560e07db5003e36895217191fc2c
simon-berner <sberner9@gmail.com>
2023-02-15 11:07:49 +0100

    display app title and modul imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GameModule } from './game/game.module';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    GameModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
