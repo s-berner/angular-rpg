130aaac81f58e300b2027aad5953584d80efe7fe
simon-berner <sberner9@gmail.com>
2023-02-21 09:53:47 +0100

    added undefined to GameElement

--- src/app/game/interfaces/GameElement.ts
+++ src/app/game/interfaces/GameElement.ts
@@ -2,4 +2,4 @@ import { Player } from '../classes/Player';
 import { Enemy } from '../classes/Enemy';
 import { Item } from '../classes/Item';
 
-export type GameElement = Player | Enemy | Item | string;
+export type GameElement = Player | Enemy | Item | undefined;
