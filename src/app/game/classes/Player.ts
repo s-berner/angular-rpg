b3ee16455c4e8f453ac197aa2e1f866fbf453d36
simon-berner <sberner9@gmail.com>
2023-02-22 10:19:07 +0100

    formatting

--- src/app/game/classes/Player.ts
+++ src/app/game/classes/Player.ts
@@ -3,19 +3,17 @@ import { Item } from './Item';
 import { ElementType } from '../enums/ElementType';
 
 export class Player extends MoveableEntity {
-    level = 1;
-    experience = 0;
-    maxHealth = 10;
-    currentHealth = this.maxHealth;
-    attributes = { strength: 5, armor: 10, agility: 10 };
-    inventory: Item[] = [];
-    type: ElementType = ElementType.Player;
+  level = 1;
+  experience = 0;
+  maxHealth = 10;
+  currentHealth = this.maxHealth;
+  attributes = { strength: 5, armor: 10, agility: 10 };
+  inventory: Item[] = [];
+  readonly type = ElementType.Player;
 
-    constructor (
-        public name: string,
-        x: number,
-        y: number,
-    ) {
-        super(x, y);
-    }
+  constructor (
+    public name: string,
+    x: number,
+    y: number,
+  ) { super(x, y); }
 }
