dd825ed155a10dc6da148089a50d3a2bdd9419bc
simon-berner <sberner9@gmail.com>
2023-02-24 11:47:21 +0100

    implemented reading route data

--- src/app/game/game-combat/game-combat.component.ts
+++ src/app/game/game-combat/game-combat.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import { Player } from '../classes/Player';
-import { Enemy } from '../classes/Enemy';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatButton } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-game-combat',
   templateUrl: './game-combat.component.html',
   styleUrls: ['./game-combat.component.css']
 })
-export class GameCombatComponent {
-  // player: Player;
-  // enemy: Enemy;
+export class GameCombatComponent implements OnInit {
+  player!: any;
+  enemy!: any;
+  test = 100;
   
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.player = JSON.parse(params['player']);
+      this.enemy = JSON.parse(params['enemy']);
+      this.player.currentHealth = 5;
+    });
+  }
+
+  attack(dmg: number): void {
+    this.test -= dmg;
+  }
 }
