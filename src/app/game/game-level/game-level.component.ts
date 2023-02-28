16ad960abb886206b799af35836b316947e347ff
simon-berner <sberner9@gmail.com>
2023-02-28 12:22:39 +0100

    little update to goToCombat Method

--- src/app/game/game-level/game-level.component.ts
+++ src/app/game/game-level/game-level.component.ts
@@ -1,13 +1,14 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { Router, NavigationExtras, NavigationEnd } from '@angular/router';
 
-import { filter, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { AngularRpg } from '../classes/AngularRpg';
 import { GameElement } from '../interfaces/GameElement';
 import { Inputs } from '../enums/Inputs';
 import { CombatService } from '../combat.service';
 import { AngularRpgService } from '../angular-rpg.service';
+import { Player } from '../classes/Player';
+import { Enemy } from '../classes/Enemy';
 
 @Component({
   selector: 'app-game-level',
@@ -17,20 +18,21 @@ import { AngularRpgService } from '../angular-rpg.service';
 export class GameLevelComponent implements OnInit {
   angularRpg!: AngularRpg;
   gameElements: GameElement[] = [];
-  gridWidth: number = 0;
-  gridHeight: number = 0;
 
   constructor(
     private router: Router,
     private combatService: CombatService,
     private angularRpgService: AngularRpgService
   ) { }
-  
+
   ngOnInit(): void {
+    console.log('ngOnInit() called in game-level.component.ts');
     this.angularRpg = this.angularRpgService.getAngularRpg();
+    const player = this.combatService.getPlayer();
+    if (player) {
+      this.angularRpg.player = player;
+    }
     this.gameElements = this.angularRpg.elements;
-    this.gridWidth = this.angularRpg.width;
-    this.gridHeight = this.angularRpg.height;
   }
 
   @HostListener('window:keydown', ['$event'])
@@ -54,11 +56,12 @@ export class GameLevelComponent implements OnInit {
         break;
     }
 
-    if(this.angularRpg.player.initiateCombat) {
-      this.goToCombat();
-      this.angularRpg.player.initiateCombat = false;
+    if(this.angularRpg.opponent) {
+      this.goToCombat(this.angularRpg.player, this.angularRpg.opponent);
+      this.angularRpg.opponent = undefined;
     }
 
+    // this forces the view to update
     let newElementsArray: GameElement[] = [];
     this.gameElements.forEach((element: GameElement) => {
       newElementsArray.push(element);
@@ -66,9 +69,9 @@ export class GameLevelComponent implements OnInit {
     this.gameElements = newElementsArray;
   }
 
-  goToCombat(): void {
-    this.combatService.setPlayer(this.angularRpg.player);
-    this.combatService.setEnemy(this.angularRpg.opponent);
+  goToCombat(player: Player, opponent: Enemy): void {
+    this.combatService.setPlayer(player);
+    this.combatService.setEnemy(opponent);
     this.router.navigate(['/game/combat']);
   }
 }
